5f9305bdd2eae04c9c3b6360da54d2529a0d0f65
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-09-30 08:01:10 +0200

    Refactor existing path operations & add update_post method with corresponding model

--- bekind_bk/main.py
+++ bekind_bk/main.py
@@ -4,33 +4,53 @@ from typing import Optional
 
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
+from typing import Union
 from pydantic import BaseModel
 
 app = FastAPI()
 
 db = {}
 
-# Pydantic Model
-class Post(BaseModel):
+# Pydantic Models
+class NewPost(BaseModel):
     title: str
     content: str
     published: bool = False
     rating: Optional[int] = None
 
 
+class PostUpdate(BaseModel):
+    title: str
+    content: str
+    published: bool
+    rating: Union[int, None]
+
+
+# Path operations
+
+
 @app.get("/")
 async def root():
+    """
+    Home route with dummy data now
+    """
     random_int = random.randint(0, 9)
     return {"random_int": f"{random_int}"}
 
 
 @app.get("/posts/")
 async def posts():
+    """
+    Get all posts, published or unpublished
+    """
     return {"data": db}
 
 
 @app.post("/posts/create", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post, response: Response):
+async def create_post(post: NewPost):
+    """
+    Create new single post using NewPost pydantic class validation and defaults
+    """
     post_id = uuid.uuid4()
     new_post = {
         "id": post_id,
@@ -44,35 +64,64 @@ async def create_post(post: Post, response: Response):
 
 
 @app.get("/posts/{id}")
-async def get_post(id, response: Response):
+async def get_post(id):
+    """
+    Get single post using id
+    """
     try:
         id = uuid.UUID(id)
         post = db[id]
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post with this id does not exist",
+        )
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid id parameter "
-        )
-    except KeyError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Post with this id does not exist",
         )
     return {"data": post}
 
 
-@app.delete("/posts/{id}/delete")
-async def delete_post(id, response: Response):
+@app.put("/posts/{id}/update")
+async def update_post(id, post: PostUpdate):
+    """
+    Update single post using id and put data
+    """
+    post_dict = post.dict()
     try:
         id = uuid.UUID(id)
-        post = db[id]
+        orig_post = db.get(id, None)
+        if orig_post is None:
+            raise KeyError
+        db[id] = post_dict
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post with this id does not exist",
+        )
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid id parameter "
         )
+    return {"data": post_dict}
+
+
+@app.delete("/posts/{id}/delete")
+async def delete_post(id):
+    """
+    Delete single post using id
+    """
+    try:
+        id = uuid.UUID(id)
+        del db[id]
     except KeyError:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Post with this id does not exist",
         )
-    del db[id]
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid id parameter "
+        )
     return {"data": "post successfuly deleted"}
