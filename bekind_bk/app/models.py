8c12302c5dba29be18648c9405c3ee1df485b76c
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-10-13 16:26:22 +0200

    Add users model

--- bekind_bk/app/models.py
+++ bekind_bk/app/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from http import server
 from sqlalchemy import Column, Integer, String, Text, Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -11,6 +12,18 @@ class Post(Base):
     title = Column(String(50), nullable=False)
     content = Column(Text, nullable=False)
     published = Column(Boolean, server_default="FALSE", nullable=False)
+    created = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class User(Base):
+    # note that user is a reserved word in postgres so using users
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
     created = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
